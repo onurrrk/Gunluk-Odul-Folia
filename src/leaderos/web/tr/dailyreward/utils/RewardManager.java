b0a1fe4f0eda942ff3086baa442e967d15d824a3
onurr_k <145716470+onurrrk@users.noreply.github.com>
2026-03-24 21:18:21 +0300

    Enhance dailyUpdate to handle streak reset condition
    Added logic to reset player streak based on configuration setting.

--- src/leaderos/web/tr/dailyreward/utils/RewardManager.java
+++ src/leaderos/web/tr/dailyreward/utils/RewardManager.java
@@ -5,9 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
-
 import org.bukkit.entity.Player;
-
 import leaderos.web.tr.dailyreward.Main;
 import leaderos.web.tr.dailyreward.utils.Gui.GunlukGui;
 import leaderos.web.tr.dailyreward.utils.objects.Cache;
@@ -17,13 +15,15 @@ public class RewardManager {
 	public static HashMap<String, Cache> cache = new HashMap<>();
 
 	public static void dailyUpdate(Player player) {
-
 		SchedulerUtils.runAsync(Main.instance, () -> {
 			Cache playerCache = RewardManager.cache.get(player.getName());
 
 			if (Main.instance.getConfig().getString("Type").equalsIgnoreCase("streak")) {
 				if (!isStreakValid(player.getName(), playerCache)) {
-					playerCache.setStreak(0);
+					boolean resetOnComplete = Main.instance.getConfig().getBoolean("ResetOnComplete", true);
+					if (resetOnComplete || playerCache.getStreak() < Main.rewards.size()) {
+						playerCache.setStreak(0);
+					}
 				}
 			}
 
@@ -59,22 +59,21 @@ public class RewardManager {
 
 	public static boolean canPlayerTakeLoot(String username) {
 		try {
+			boolean resetOnComplete = Main.instance.getConfig().getBoolean("ResetOnComplete", true);
+			if (!resetOnComplete && cache.get(username).getStreak() >= Main.rewards.size()) {
+				return false;
+			}
+
 			long unix = System.currentTimeMillis() / 1000L;
 
 			if (cache.get(username).getLastTakeUnix() < unix) {
-
 				SimpleDateFormat sdformat = new SimpleDateFormat("dd-MM-yyyy");
-
 				Date lastTake = sdformat.parse(cache.get(username).getLastTake());
-
 				Date now = new Date();
-
 				String nowString = sdformat.format(now);
-
 				Date actualNow = sdformat.parse(nowString);
 
 				return lastTake.compareTo(actualNow) < 0;
-
 			} else {
 				return false;
 			}
